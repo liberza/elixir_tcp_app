862fb87f376035e5b84ba26f88006175367bdabd
Nick Levesque <nick@levesque.io>
2018-09-25 21:35:58 -0400

    Clarify comments in type.ex

--- lib/tcp_app/type.ex
+++ lib/tcp_app/type.ex
@@ -1,6 +1,8 @@
 defmodule TcpApp.Type do
   # TODO: write a macro to directly map the integer types to each
   # format in TcpApp.Format, for a speed boost.
+  # Right now we translate from message ID -> ID atom -> format key,
+  # and we could actually just go message ID -> format key.
 
   @msg_types %{
     # Example message types
