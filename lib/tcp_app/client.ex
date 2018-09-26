88dda49b617752d220966f79c6331b78e0b864e2
Nick Levesque <nick@levesque.io>
2018-09-25 23:10:48 -0400

    Added a working example / fixed errors from a refactor...

--- lib/tcp_app/client.ex
+++ lib/tcp_app/client.ex
@@ -2,31 +2,53 @@ defmodule TcpApp.Client do
   alias TcpApp.{Type}
   alias TcpApp.Protocol.{Header, Payload}
 
+  # A test of our parser. Build a message manually, then parse & print it.
   def try_it() do
-    # Create an example stream here and parse with
-    # print_msg_from_stream/1
+    payload =
+      <<
+        "S",
+        String.length("hello world") :: 32-little,
+        "hello world",
+        "W",
+        100 :: 16-little-unsigned-integer,
+        "D",
+        200 :: 32-little-unsigned-integer
+      >>
+    header =
+      <<
+        0001 :: 16-big,
+        0 :: 32-big,
+        byte_size(payload) :: 32-little
+      >>
+
+    message = header <> payload
+    
+    {:ok, stream} = StringIO.open(message)
+
+    print_msg_from_stream(stream)
   end
   
   def print_msg_from_stream(stream) do
-    header = IO.binread(stream, Header.header_size) 
-    |> Header.parse
+    {:ok, header, _rest} = IO.binread(stream, Header.header_size) |> Header.parse
     print_header(header)
 
     IO.binread(stream, header[:payload_size]) 
-    |> Payload.parse(header.type, &Type.get_format/1)
+    |> Payload.parse(header[:type], &Type.get_format/1)
     |> print_payload()
 
     stream
   end
 
   @doc "Prints header info for one Header."
-  def print_header({:ok, header, _rest}) do
-    IO.puts("Type:  #{header.type}")
-    IO.puts("Size:  #{header.size}")
-    IO.puts("Flags: #{header.flags}")
+  def print_header(header) do
+    IO.puts("Type:  #{header[:type]}")
+    IO.puts("Size:  #{header[:payload_size]}")
+    IO.puts("Flags: #{header[:flags]}")
   end
 
   @doc "Prints contents of a payload."
+  def print_payload([]) do end
+  
   def print_payload([var | t]) do
     IO.puts("  #{elem(var, 0)}: #{elem(var, 1)}")
     print_payload(t)
